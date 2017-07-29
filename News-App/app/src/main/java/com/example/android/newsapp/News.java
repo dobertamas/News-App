986421c755ba1ad028f0cbe05fc47e9dbb197281
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-28 19:50:39 -0700

    Adding ViewHolder, updating list item, adding News fields

--- News-App/app/src/main/java/com/example/android/newsapp/News.java
+++ News-App/app/src/main/java/com/example/android/newsapp/News.java
@@ -1,4 +1,28 @@
 package com.example.android.newsapp;
 
 public class News {
+
+    private String mTitle;
+    private String mUrl;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public News(String title, String url) {
+        mTitle = title;
+        mUrl = url;
+    }
 }
--- News-App/app/src/main/java/com/example/android/newsapp/NewsAdapter.java
+++ News-App/app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -3,10 +3,27 @@ package com.example.android.newsapp;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class NewsAdapter extends ArrayAdapter<News> {
-    public NewsAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull News[] objects) {
-        super(context, resource, objects);
+
+    private static final String LOG_TAG = NewsAdapter.class.getSimpleName();
+
+    public NewsAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull News[] newsArray) {
+        super(context, 0, newsArray);
     }
+
+    private class ViewHolder {
+        TextView titleTextView;
+        TextView urlTextView;
+
+        ViewHolder(View view) {
+            this.titleTextView = view.findViewById(R.id.list_item_title);
+            this.urlTextView = view.findViewById(R.id.list_item_url);
+        }
+    }
+
+
 }
