681c55683272e593ff6a61eb6cdc7791234bcc93
Tamas Dober <tamas.dober@mobileforming.com>
2017-07-28 20:07:14 -0700

    Finishing and setting adapter

--- News-App/app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ News-App/app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.list) ListView mListView;
+
+    News[] mNewsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        mNewsList = new News[]{new News("title1", "url1"), new News("title2", "url2")};
+
+        NewsAdapter newsAdapter = new NewsAdapter(this, 0, mNewsList);
+
+        mListView.setAdapter(newsAdapter);
+
+
     }
 }
--- News-App/app/src/main/java/com/example/android/newsapp/NewsAdapter.java
+++ News-App/app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -3,7 +3,10 @@ package com.example.android.newsapp;
 import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -13,6 +16,34 @@ public class NewsAdapter extends ArrayAdapter<News> {
 
     public NewsAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull News[] newsArray) {
         super(context, 0, newsArray);
+    }
+
+    @NonNull @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        News news = getItem(position);
+
+
+        // Populate the data into the view using the data object
+        assert news != null;
+        viewHolder.titleTextView.setText(news.getTitle());
+        viewHolder.urlTextView.setText(news.getUrl());
+
+        // Return the completed view to render on screen
+        return convertView;
     }
 
     private class ViewHolder {
